53c70d7764127878119d5955f01368e2c413beb6
asaf <asaf@flexi-core.com>
2020-06-24 01:08:47 +0300

    4.0.8 - support for nested contexts , context dependency is now resolved using custom bean factory and custom application context

--- src/main/java/com/flexicore/init/FlexiCoreExtensionFactory.java
+++ src/main/java/com/flexicore/init/FlexiCoreExtensionFactory.java
@@ -4,22 +4,20 @@ import org.pf4j.PluginWrapper;
 import org.pf4j.spring.SpringExtensionFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Queue;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.stream.Collectors;
 
 public class FlexiCoreExtensionFactory extends SpringExtensionFactory {
-    private FlexiCorePluginManager pluginManager;
-    private Map<String, ApplicationContext> contextCache = new ConcurrentHashMap<>();
-    private Queue<ApplicationContext> leafContexts = new LinkedBlockingQueue<>();
-    private Logger logger = LoggerFactory.getLogger(FlexiCoreExtensionFactory.class);
+    private final FlexiCorePluginManager pluginManager;
+    private final Map<String, FlexiCoreApplicationContext> contextCache = new ConcurrentHashMap<>();
+    private final Queue<ApplicationContext> leafContexts = new LinkedBlockingQueue<>();
+    private final Logger logger = LoggerFactory.getLogger(FlexiCoreExtensionFactory.class);
 
 
     public FlexiCoreExtensionFactory(FlexiCorePluginManager pluginManager) {
@@ -42,84 +40,43 @@ public class FlexiCoreExtensionFactory extends SpringExtensionFactory {
 
     }
 
+
     public ApplicationContext getApplicationContext(PluginWrapper pluginWrapper) {
         String pluginId = pluginWrapper!=null?pluginWrapper.getPluginId():"core-extensions";
-        ApplicationContext applicationContext = contextCache.get(pluginId);
+        FlexiCoreApplicationContext applicationContext = contextCache.get(pluginId);
         if (applicationContext == null) {
             applicationContext = createApplicationContext(pluginWrapper);
             contextCache.put(pluginId, applicationContext);
+            List<String> dependencies = pluginWrapper!=null?pluginWrapper.getDescriptor().getDependencies().parallelStream().map(f -> f.getPluginId()).sorted().collect(Collectors.toList()):new ArrayList<>();
+            List<ApplicationContext> dependenciesContexts=dependencies.stream().map(f->pluginManager.getPlugin(f)).filter(f->f!=null).map(this::getApplicationContext).collect(Collectors.toList());
+            applicationContext.getAutowireCapableBeanFactory().addDependenciesContext(dependenciesContexts);
+            applicationContext.refresh();
+
+            leafContexts.removeAll(dependenciesContexts);
+            leafContexts.add(applicationContext);
         }
         return applicationContext;
     }
 
-    private ApplicationContext createApplicationContext(PluginWrapper pluginWrapper) {
-        ApplicationContext parentContext = getParentContext(pluginWrapper);
-        return createGenericApplicationContext(pluginWrapper, parentContext);
-
-    }
-
-    private ApplicationContext createGenericApplicationContext(PluginWrapper pluginWrapper, ApplicationContext parent) {
+    private FlexiCoreApplicationContext createApplicationContext(PluginWrapper pluginWrapper) {
         String pluginId = pluginWrapper!=null?pluginWrapper.getPluginId():null;
         List<Class<? extends com.flexicore.interfaces.Plugin>> beanClasses = pluginManager.getExtensionClasses(com.flexicore.interfaces.Plugin.class, pluginId);
         ClassLoader pluginClassLoader = pluginWrapper!=null?pluginWrapper.getPluginClassLoader():Thread.currentThread().getContextClassLoader();
-        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
-        applicationContext.setParent(parent);
+        FlexiCoreApplicationContext applicationContext = new FlexiCoreApplicationContext();
+        applicationContext.setParent(pluginManager.getApplicationContext());
         applicationContext.setClassLoader(pluginClassLoader);
         for (Class<? extends com.flexicore.interfaces.Plugin> beanClass : beanClasses) {
             applicationContext.register(beanClass);
         }
-        applicationContext.refresh();
-        leafContexts.remove(parent);
-        leafContexts.add(applicationContext);
 
         return applicationContext;
+
     }
 
 
-    private ApplicationContext getParentContext(PluginWrapper pluginWrapper) {
-
-        List<String> dependencies = pluginWrapper!=null?pluginWrapper.getDescriptor().getDependencies().parallelStream().map(f -> f.getPluginId()).sorted().collect(Collectors.toList()):new ArrayList<>();
-        if (dependencies.isEmpty()) {
-            return pluginManager.getApplicationContext();
-        }
-        String cacheKey = dependencies.stream().collect(Collectors.joining("|"));
-        ApplicationContext parentContext = contextCache.get(cacheKey);
-        if (parentContext == null) {
-            parentContext = createParentContext(pluginWrapper, dependencies);
-            contextCache.put(cacheKey, parentContext);
-        }
-        return parentContext;
-    }
-
-    private ApplicationContext createParentContext(PluginWrapper pluginWrapper, List<String> dependencies) {
-        List<AnnotationConfigApplicationContext> parents = dependencies.stream().map(f -> (AnnotationConfigApplicationContext) getApplicationContext(pluginManager.getPlugin(f))).collect(Collectors.toList());
-        leafContexts.removeAll(parents);
-        if (parents.size() == 1) {
-            return parents.get(0);
-        }
-        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
-        applicationContext.setParent(pluginManager.getApplicationContext());
-        applicationContext.setClassLoader(pluginWrapper.getPluginClassLoader());
-        for (AnnotationConfigApplicationContext parent : parents) {
-            for (String beanDefinitionName : parent.getBeanDefinitionNames()) {
-                Class<?> c = parent.getType(beanDefinitionName);
-                if (c != null) {
-                    PluginWrapper wrapper = pluginManager.whichPlugin(c);
-                    if (wrapper != null) {
-                        applicationContext.registerBeanDefinition(beanDefinitionName, parent.getBeanDefinition(beanDefinitionName));
-                    }
-
-                }
-
-
-            }
-        }
-
-        applicationContext.refresh();
-        return applicationContext;
-    }
 
     public Queue<ApplicationContext> getLeafContexts() {
         return leafContexts;
     }
+
 }
