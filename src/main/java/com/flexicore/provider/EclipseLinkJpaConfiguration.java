8d5ae2e6de5786232322b7ba152b571b2b6befdb
asaf <asaf@flexi-core.com>
2020-09-20 17:09:42 +0300

    4.0.23 - added inheritance configuration support

--- src/main/java/com/flexicore/provider/EclipseLinkJpaConfiguration.java
+++ src/main/java/com/flexicore/provider/EclipseLinkJpaConfiguration.java
@@ -2,14 +2,12 @@ package com.flexicore.provider;
 
 import com.flexicore.converters.JsonConverter;
 import com.flexicore.model.Baseclass;
-import org.reflections.Reflections;
-import org.reflections.util.ConfigurationBuilder;
+import org.eclipse.persistence.internal.jpa.metadata.accessors.classes.EntityAccessor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.orm.jpa.JpaBaseConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.JpaProperties;
 import org.springframework.boot.autoconfigure.transaction.TransactionManagerCustomizers;
@@ -29,18 +27,15 @@ import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.transaction.jta.JtaTransactionManager;
 
-import javax.persistence.Entity;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 import javax.sql.DataSource;
-import java.io.File;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+import java.lang.reflect.Proxy;
 import java.net.MalformedURLException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URL;
 import java.util.*;
-import java.util.concurrent.ConcurrentSkipListSet;
-import java.util.logging.Level;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
 @Configuration
@@ -48,10 +43,12 @@ import java.util.stream.StreamSupport;
 public class EclipseLinkJpaConfiguration extends JpaBaseConfiguration {
 
 
-    private Logger logger=LoggerFactory.getLogger(EclipseLinkJpaConfiguration.class);
+    private Logger logger = LoggerFactory.getLogger(EclipseLinkJpaConfiguration.class);
 
     @Value("${eclipselink.ddl-generation}")
     private String ddlGeneration;
+    @Value("${flexicore.inheritence.strategy:SINGLE_TABLE}")
+    private String inheritanceType;
 
     @Autowired
     Environment env;
@@ -59,7 +56,6 @@ public class EclipseLinkJpaConfiguration extends JpaBaseConfiguration {
 
     protected EclipseLinkJpaConfiguration(DataSource dataSource, JpaProperties properties, ObjectProvider<JtaTransactionManager> jtaTransactionManager) {
         super(dataSource, properties, jtaTransactionManager);
-
 
 
     }
@@ -78,21 +74,24 @@ public class EclipseLinkJpaConfiguration extends JpaBaseConfiguration {
 
     @Bean
     @Primary
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory(final EntityManagerFactoryBuilder builder, @Autowired DataSource dataSource,@Autowired EntitiesHolder entitiesHolder) throws ClassNotFoundException, MalformedURLException {
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(final EntityManagerFactoryBuilder builder, @Autowired DataSource dataSource, @Autowired EntitiesHolder entitiesHolder) throws ClassNotFoundException, MalformedURLException {
         Set<Class<?>> entities = new HashSet<>(entitiesHolder.getEntities());
         entities.add(JsonConverter.class);
-        logger.debug("Discovered Entities: "+entities.stream().map(f->f.getCanonicalName()).collect(Collectors.joining(System.lineSeparator())));
-        Class<?>[] entitiesArr=new Class<?>[entities.size()];
+        logger.debug("Discovered Entities: " + entities.stream().map(f -> f.getCanonicalName()).collect(Collectors.joining(System.lineSeparator())));
+        Class<?>[] entitiesArr = new Class<?>[entities.size()];
         entities.toArray(entitiesArr);
+        String ormLocation = InheritanceType.JOINED.name().equals(inheritanceType) ? "META-INF/joined-orm.xml" : "META-INF/single-table-orm.xml";
         EntityManagerFactoryBuilder.Builder primary = builder.dataSource(dataSource)
                 .packages(entitiesArr)
                 .persistenceUnit("primary")
                 .properties(getVendorProperties())
+                .mappingResources(ormLocation)
                 .jta(true);
         final LocalContainerEntityManagerFactoryBean ret = primary.build();
 
         return ret;
     }
+
 
 
 
@@ -103,7 +102,7 @@ public class EclipseLinkJpaConfiguration extends JpaBaseConfiguration {
 
     @Override
     protected Map<String, Object> getVendorProperties() {
-        Map<String,Object> props=new HashMap<>();
+        Map<String, Object> props = new HashMap<>();
         props.put("javax.persistence.schema-generation.create-database-schemas", "true");
         //add all properties starting with eclipselink to vendor properties
         MutablePropertySources propSrcs = ((AbstractEnvironment) env).getPropertySources();
@@ -111,10 +110,11 @@ public class EclipseLinkJpaConfiguration extends JpaBaseConfiguration {
                 .filter(ps -> ps instanceof EnumerablePropertySource)
                 .map(ps -> ((EnumerablePropertySource) ps).getPropertyNames())
                 .flatMap(Arrays::stream)
-                .filter(f->f.startsWith("eclipselink"))
+                .filter(f -> f.startsWith("eclipselink"))
                 .forEach(propName -> props.put(propName, env.getProperty(propName)));
         return props;
     }
+
 
 
 
