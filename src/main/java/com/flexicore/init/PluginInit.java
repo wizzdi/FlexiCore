ea4d91c012a7b04c46ddfc3f735242a04c2be763
asaf <asaf@flexi-core.com>
2020-10-29 11:45:22 +0200

    5.0.0 base service seperation

--- src/main/java/com/flexicore/init/PluginInit.java
+++ src/main/java/com/flexicore/init/PluginInit.java
@@ -2,13 +2,10 @@ package com.flexicore.init;
 
 import com.flexicore.data.jsoncontainers.CrossLoaderResolver;
 import com.flexicore.events.PluginsLoadedEvent;
-import com.flexicore.interfaces.*;
-import com.flexicore.rest.JaxRsActivator;
 import org.pf4j.PluginManager;
 import org.pf4j.PluginWrapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.aop.aspectj.annotation.AspectJProxyFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.boot.actuate.health.HealthContributor;
@@ -20,8 +17,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Scope;
 
-import javax.ws.rs.ext.Provider;
-import java.lang.reflect.Constructor;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
@@ -57,30 +52,11 @@ public class PluginInit {
     public PluginsLoadedEvent initPluginLoader() {
         CrossLoaderResolver.registerClassLoader( pluginManager.getStartedPlugins().stream().map(f->f.getPluginClassLoader()).collect(Collectors.toList()));
         List<PluginWrapper> startedPlugins = pluginManager.getStartedPlugins().stream().sorted(PLUGIN_COMPARATOR_FOR_REST).collect(Collectors.toList());
-        List<? extends AspectPlugin> aspects = pluginManager.getExtensions(AspectPlugin.class);
 
         long startedAll=System.currentTimeMillis();
         //makes sure older versions are loaded first so default version for apis is being taken from oldest plugin
         for (PluginWrapper startedPlugin : startedPlugins) {
-            long start = System.currentTimeMillis();
-            logger.info("REST Registration handling plugin: " + startedPlugin);
 
-            List<? extends RestServicePlugin> restPlugins = pluginManager.getExtensions(RestServicePlugin.class, startedPlugin.getPluginId());
-            for (RestServicePlugin plugin : restPlugins) {
-
-                logger.info("REST class " + plugin);
-                try {
-                    AspectJProxyFactory factory = new AspectJProxyFactory(plugin);
-                    for (AspectPlugin aspect : aspects) {
-                        factory.addAspect(aspect);
-                    }
-                    factory.setProxyTargetClass(true);
-                    Object proxy = factory.getProxy(plugin.getClass().getClassLoader());
-                    JaxRsActivator.addSingletones(proxy);
-                } catch (Exception e) {
-                    logger.error("Failed registering REST service " + plugin.getClass(), e);
-                }
-            }
 
             List<? extends HealthContributor> healthContributors = pluginManager.getExtensions(HealthContributor.class, startedPlugin.getPluginId());
             for (HealthContributor healthContributor : healthContributors) {
@@ -95,11 +71,7 @@ public class PluginInit {
                 }
             }
 
-            List<JaxRSProviderPlugin> classes = pluginManager.getExtensions(JaxRSProviderPlugin.class, startedPlugin.getPluginId());
-            for (JaxRSProviderPlugin jaxRSProviderPlugin : classes) {
-                JaxRsActivator.addSingletones(jaxRSProviderPlugin);
-                logger.debug("registered JAX-RS provider "+jaxRSProviderPlugin);
-            }
+
 
 
 
