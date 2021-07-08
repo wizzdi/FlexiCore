d0fd98da81404df0f7e50602ed6f0ec1b181869b
Asaf Ben Natan <asaf@wizzdi.com>
2021-07-08 12:57:44 +0300

    5.3.0 - updated dependencies

--- src/main/java/com/wizzdi/flexicore/init/FlexiCoreApplication.java
+++ src/main/java/com/wizzdi/flexicore/init/FlexiCoreApplication.java
@@ -16,6 +16,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.ApplicationPidFileWriter;
+import org.springframework.boot.web.embedded.undertow.UndertowDeploymentInfoCustomizer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
