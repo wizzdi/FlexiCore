294113dd8a79c60a343af99565a7a70b1e56f9cc
Asaf Ben Natan <asaf@wizzdi.com>
2024-03-13 16:04:15 +0200

    allowing flyway migrations to be disabled

--- src/main/java/com/wizzdi/flexicore/init/config/FlywayConfiguration.java
+++ src/main/java/com/wizzdi/flexicore/init/config/FlywayConfiguration.java
@@ -3,19 +3,27 @@ package com.wizzdi.flexicore.init.config;
 import org.flywaydb.core.Flyway;
 import org.flywaydb.core.api.migration.JavaMigration;
 import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import jakarta.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.stream.Collectors;
 
 @Configuration
 public class FlywayConfiguration {
 
+    @Value("${flexicore.flyway.enable:true}")
+    private boolean enabled;
+
     @Bean
     public FlywayObjectHolder flywayObjectHolder(DataSource dataSource, ObjectProvider<JavaMigration> javaMigrationsProvider, EntityManagerFactory entityManagerFactory) {
+        if(!enabled){
+            return new FlywayObjectHolder(Collections.emptySet());
+        }
         JavaMigration[] javaMigrations = javaMigrationsProvider.stream().toArray(JavaMigration[]::new);
         Flyway flyway = Flyway.configure().javaMigrations(javaMigrations).baselineOnMigrate(true).dataSource(dataSource).load();
         flyway.migrate();
